ad22aa63b3829886b5127060b600cd16899b4f12
Денис Агапов <cstud110911@vyatsu.ru>
2020-05-14 19:28:57 +0300

    commit client ui + code #1

---
+++ Esoft_Project/FormClient.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Esoft_Project
+{
+    partial class FormClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxMiddleName = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.labelMiddleName = new System.Windows.Forms.Label();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.labelEmai = new System.Windows.Forms.Label();
+            this.listViewClient = new System.Windows.Forms.ListView();
+            this.Id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Имя = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Фамилия = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Отчество = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Телефон = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Почта = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(24, 37);
+            this.textBoxFirstName.Multiline = true;
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(127, 24);
+            this.textBoxFirstName.TabIndex = 0;
+            this.textBoxFirstName.TextChanged += new System.EventHandler(this.textBoxFirstName_TextChanged);
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(24, 123);
+            this.textBoxLastName.Multiline = true;
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(127, 24);
+            this.textBoxLastName.TabIndex = 0;
+            // 
+            // textBoxMiddleName
+            // 
+            this.textBoxMiddleName.Location = new System.Drawing.Point(24, 80);
+            this.textBoxMiddleName.Multiline = true;
+            this.textBoxMiddleName.Name = "textBoxMiddleName";
+            this.textBoxMiddleName.Size = new System.Drawing.Size(127, 24);
+            this.textBoxMiddleName.TabIndex = 0;
+            this.textBoxMiddleName.TextChanged += new System.EventHandler(this.textBoxMiddleName_TextChanged);
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(24, 166);
+            this.textBoxPhone.Multiline = true;
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(127, 24);
+            this.textBoxPhone.TabIndex = 0;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(24, 209);
+            this.textBoxEmail.Multiline = true;
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(127, 24);
+            this.textBoxEmail.TabIndex = 0;
+            // 
+            // labelMiddleName
+            // 
+            this.labelMiddleName.AutoSize = true;
+            this.labelMiddleName.Location = new System.Drawing.Point(21, 64);
+            this.labelMiddleName.Name = "labelMiddleName";
+            this.labelMiddleName.Size = new System.Drawing.Size(54, 13);
+            this.labelMiddleName.TabIndex = 1;
+            this.labelMiddleName.Text = "Отчество";
+            this.labelMiddleName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(21, 21);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(29, 13);
+            this.labelFirstName.TabIndex = 1;
+            this.labelFirstName.Text = "Имя";
+            this.labelFirstName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(21, 107);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(56, 13);
+            this.labelLastName.TabIndex = 1;
+            this.labelLastName.Text = "Фамилия";
+            this.labelLastName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Location = new System.Drawing.Point(21, 150);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(52, 13);
+            this.labelPhone.TabIndex = 1;
+            this.labelPhone.Text = "Телефон";
+            this.labelPhone.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelEmai
+            // 
+            this.labelEmai.AutoSize = true;
+            this.labelEmai.Location = new System.Drawing.Point(21, 193);
+            this.labelEmai.Name = "labelEmai";
+            this.labelEmai.Size = new System.Drawing.Size(37, 13);
+            this.labelEmai.TabIndex = 1;
+            this.labelEmai.Text = "Почта";
+            this.labelEmai.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listViewClient
+            // 
+            this.listViewClient.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Id,
+            this.Имя,
+            this.Фамилия,
+            this.Отчество,
+            this.Телефон,
+            this.Почта});
+            this.listViewClient.FullRowSelect = true;
+            this.listViewClient.GridLines = true;
+            this.listViewClient.HideSelection = false;
+            this.listViewClient.Location = new System.Drawing.Point(157, 21);
+            this.listViewClient.MultiSelect = false;
+            this.listViewClient.Name = "listViewClient";
+            this.listViewClient.Size = new System.Drawing.Size(515, 212);
+            this.listViewClient.TabIndex = 2;
+            this.listViewClient.UseCompatibleStateImageBehavior = false;
+            this.listViewClient.View = System.Windows.Forms.View.Details;
+            this.listViewClient.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Id
+            // 
+            this.Id.Text = "Id";
+            // 
+            // Имя
+            // 
+            this.Имя.Text = "Имя";
+            // 
+            // Фамилия
+            // 
+            this.Фамилия.Text = "Фамилия";
+            // 
+            // Отчество
+            // 
+            this.Отчество.Text = "Отчество";
+            // 
+            // Телефон
+            // 
+            this.Телефон.Text = "Телефон";
+            // 
+            // Почта
+            // 
+            this.Почта.Text = "Почта";
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(510, 239);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(78, 34);
+            this.buttonEdit.TabIndex = 3;
+            this.buttonEdit.Text = "Изменить";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(426, 239);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(78, 34);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Создать";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.Location = new System.Drawing.Point(594, 239);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(78, 34);
+            this.buttonDel.TabIndex = 3;
+            this.buttonDel.Text = "Удалить";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            this.buttonDel.Click += new System.EventHandler(this.buttonDel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Esoft_Project.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(191, 297);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(323, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 462);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.listViewClient);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.labelEmai);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelMiddleName);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.textBoxMiddleName);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Name = "FormClient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Клиенты";
+            this.Load += new System.EventHandler(this.FormClient_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxMiddleName;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label labelMiddleName;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Label labelEmai;
+        private System.Windows.Forms.ListView listViewClient;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.ColumnHeader Id;
+        private System.Windows.Forms.ColumnHeader Имя;
+        private System.Windows.Forms.ColumnHeader Фамилия;
+        private System.Windows.Forms.ColumnHeader Отчество;
+        private System.Windows.Forms.ColumnHeader Телефон;
+        private System.Windows.Forms.ColumnHeader Почта;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
--- Esoft_Project/Program.cs
+++ Esoft_Project/Program.cs
@@ -8,6 +8,8 @@ namespace Esoft_Project
 {
     static class Program
     {
+        //создание статического экземпляра класса модели ADO.EDM
+        public static WFTutorialEntities2 wftDb = new WFTutorialEntities2();
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
@@ -16,7 +18,7 @@ namespace Esoft_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
     }
 }
